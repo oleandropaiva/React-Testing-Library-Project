8ce4a40bc8cfb152eb6d84e04602cf09354b1516
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-13 11:00:40 -0300

    item 01 em andamento

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('testando se o link app.js possui o texto home', () => {
+  test('primeiro link deve possuir o texto Home', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkElement = screen.getByText(/home/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+/* describe('testando se o link app.js possui o texto home', () => {
+  it('Renderiza o app na tela', async () => {
+    render (
+    <MemoryRouter>
+  (<App />);
+     </MemoryRouter>
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeInTheDocument();
+  });
+}); */
