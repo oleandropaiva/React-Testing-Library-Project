bf7922783e0581af5b27914c86b56f247924e5a0
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-13 18:29:25 -0300

    Favorite Pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste no componente FavoritePokemons', () => {
+  test('Testa exibe uma mensagem , caso a pessoa não tenha pokémons favoritos.', () => {
+    render(<FavoritePokemons />);
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
