6407ee3d2ae61fbeff3ee1c367a636d735483ed8
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-14 00:50:56 -0300

    NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -14,8 +14,11 @@ describe('Teste no componente NotFound', () => {
   });
 
   test('Testa se heading h2 contém o texto Page requested not found 😭', () => {
-    const textH2 = screen.getByRole('heading',
-      /'Page requested not found 😭'/i);
+    const textH2 = screen.getByRole('heading', { level: 2 });
     expect(textH2).toBeInTheDocument();
+
+    const textH2Text = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+    expect(textH2Text).toBeInTheDocument();
   });
 });
