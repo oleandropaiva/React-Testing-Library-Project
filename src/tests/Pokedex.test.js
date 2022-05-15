147ddb1b97d141e409b02769688aee56651d00e6
Leandro Paiva <leandroapaiva@yahoo.com>
2022-05-15 00:22:09 -0300

    test button próximo pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Teste no componente Pokedex', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
   test('Teste se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    const textHeading = screen.getByRole('heading', { name: 'Encountered pokémons' });
+    expect(textHeading).toBeInTheDocument();
+  });
+  test('Testa se é exibido o próximo pokémon da lista quando o botão  é clicado', () => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
-    const textHeading = screen.getByRole('heading', { name: 'Encountered pokémons' });
-    expect(textHeading).toBeInTheDocument();
+    const button = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
+    const textButton = screen.getByText(/charmander/i);
+    expect(textButton).toBeInTheDocument();
   });
 });
